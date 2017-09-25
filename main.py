3028a449664736777a5286850c8551eef607b14b
Nagy Péter <peternagy1332@sch.bme.hu>
2017-09-25 23:50:03 +0200

    Unified vocabulary for roots and tags

--- main.py
+++ main.py
@@ -8,49 +8,29 @@ import re
 
 class MorphDisamTrainer(object):
     def __init__(self, train_files_paths, window_size=5):
-        Vocabularies = namedtuple('Vocabularies', ['tags', 'morphemes'])
-        self.__vocabularies = Vocabularies(
-            tags = self.__build_vocabulary_from_file(train_files_paths.tags),
-            morphemes = self.__build_vocabulary_from_file(train_files_paths.morphemes),
-        )
-
+        self.__vocabulary = self.__read_features_to_vocabulary(train_files_paths.tags, train_files_paths.morphemes)
         self.__corpus_dataframe = self.__read_corpus_dataframe(train_files_paths.corpus)
-
         self.__window_size = window_size
 
-    @staticmethod
-    def __build_vocabulary_from_file(file_path):
-        with open(file_path, encoding='utf-8') as f:
-            tags = f.read().splitlines()
-            vocabulary = dict(zip(tags, range(1, len(tags) + 1)))
-        return vocabulary
+    def __read_features_to_vocabulary(self, file_tags, file_morphemes):
+        features = []
+        with open(file_tags, encoding='utf-8') as f: features.extend(f.read().splitlines())
+        with open(file_morphemes, encoding='utf-8') as f: features.extend(f.read().splitlines())
+        return dict(zip(features, range(1, len(features) + 1)))
 
-    @staticmethod
-    def __read_corpus_dataframe(path_corpuses):
+    def __read_corpus_dataframe(self, path_corpuses):
         return pd.concat(
-            (pd.read_csv(f,
-                         sep='\t',
-                         usecols=[4],
-                         skip_blank_lines=True,
-                         header=None,
-                         nrows=2,
-                         names=['analyses'])
+            (pd.read_csv(f, sep='\t', usecols=[4], skip_blank_lines=True, header=None, nrows=2, names=['analyses'])
              for f in glob.glob(path_corpuses)))
 
     def __analysis_to_vector(self, analysis):
         root = re.search(r'\w+', analysis, re.UNICODE).group(0)
         tags = re.findall(r'\[[^]]*\]', analysis, re.UNICODE)
-
-        vector = [self.__vocabularies.morphemes[root]]
-        vector.extend([self.__vocabularies.tags[tag] for tag in tags])
-
-        return np.matrix(vector)
+        return np.matrix([self.__vocabulary[root]] + [self.__vocabulary[tag] for tag in tags])
 
     def generate_batch(self, batch_size):
         for analysis in self.__corpus_dataframe['analyses']:
             print(self.__analysis_to_vector(analysis))
-
-
 
 
 def main():
