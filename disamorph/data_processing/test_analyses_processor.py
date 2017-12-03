4a0cbaaaa0f9506151322d32cf71861a5674d277
Nagy Péter <peternagy1332@sch.bme.hu>
2017-12-03 19:09:55 +0100

    Unit testing environment: initial setup.

---
+++ disamorph/data_processing/test_analyses_processor.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestAnalysesProcessor(unittest.TestCase):
+
+    def test_isupper(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
